b2b27872feb688422469d7cf1e34208bc258c46f
pauldidonatojr <pjdidonatojr@gmail.com>
2022-04-29 18:20:52 -0400

    fixed first function

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -3,6 +3,6 @@ exports.handler = async function handler(event, context) {
     console.log(event)
     return {
         statusCode: 200,
-        body: 'Netlify Functions',
+        body: 'Netlify Functions 1',
     }
 }
